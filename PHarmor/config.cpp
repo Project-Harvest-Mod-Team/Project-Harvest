db2960afcc5792dd731c4e087c2a3c245b2730c1
Tom Soldner <tomsoldner4@gmail.com>
2022-05-05 00:46:21 -0400

    Added Burt & Lafayette's armor

--- PHarmor/config.cpp
+++ PHarmor/config.cpp
@@ -384,7 +384,7 @@ class CfgWeapons
 		hiddenSelectionsTextures[] =
 		{
 			"PHarmor\data\helmets\customhelmet_Slice_co.paa",
-			"PHarmor\data\helmets\customvisor_soldner_co.paa"
+			"PHarmor\data\helmets\customvisor_slice_co.paa"
 		};
 	};
 	class Slice_ODST_Helmet_dp : OPTRE_UNSC_CH252D_Helmet_dp
@@ -433,6 +433,39 @@ class CfgWeapons
 		hiddenSelectionsTextures[] =
 		{
 			"PHarmor\data\helmets\customhelmet_lafayette_co.paa",
+			"PHarmor\data\helmets\ODST_Helmet_Visor_ca.paa"
+		};
+	};
+	class Burtovski_ODST_Helmet : OPTRE_UNSC_CH252D_Helmet
+	{
+		dlc = "PHarmor";
+		scope = 2;
+		author = "Project Harvest Mod Team";
+		displayName = "CH252D Helmet (Burtovski)";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"PHarmor\data\helmets\customhelmet_burtovski_co.paa",
+			"PHarmor\data\helmets\customvisor_base_co.paa"
+		};
+	};
+	class Burtovski_ODST_Helmet_dp : OPTRE_UNSC_CH252D_Helmet_dp
+	{
+		dlc = "PHarmor";
+		author = "Project Harvest Mod Team";
+		displayName = "CH252D Helmet (Burtovski)";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"PHarmor\data\helmets\customhelmet_burtovski_co.paa",
 			"PHarmor\data\helmets\ODST_Helmet_Visor_ca.paa"
 		};
 	};
@@ -2747,7 +2780,7 @@ class CfgWeapons
 			//"A_ODST", 
 			"A_ShinArmorLeft", 
 			"A_ShinArmorRight", 
-			//"A_TacPad", 
+			"A_TacPad", 
 			//"A_ThighArmorLeft", 
 			//"A_ThighArmorRight", 
 			//"AS_BaseLeft", 
@@ -2813,7 +2846,7 @@ class CfgWeapons
 				//"A_ODST", 
 				"A_ShinArmorLeft", 
 				"A_ShinArmorRight", 
-				//"A_TacPad", 
+				"A_TacPad", 
 				//"A_ThighArmorLeft", 
 				//"A_ThighArmorRight", 
 				//"AS_BaseLeft", 
@@ -3295,7 +3328,7 @@ class CfgWeapons
 			//"A_ODST", 
 			//"A_ShinArmorLeft", 
 			//"A_ShinArmorRight", 
-			"A_TacPad", 
+			//"A_TacPad", 
 			//"A_ThighArmorLeft", 
 			//"A_ThighArmorRight", 
 			//"AS_BaseLeft", 
@@ -3361,7 +3394,7 @@ class CfgWeapons
 				//"A_ODST", 
 				//"A_ShinArmorLeft", 
 				//"A_ShinArmorRight", 
-				"A_TacPad", 
+				//"A_TacPad", 
 				//"A_ThighArmorLeft", 
 				//"A_ThighArmorRight", 
 				//"AS_BaseLeft", 
@@ -3407,6 +3440,143 @@ class CfgWeapons
 				"PHarmor\data\armor\legs_base_co.paa",
 				"PHarmor\data\army\data\ghillie_woodland_co.paa",
 				"PHarmor\data\armor\customarmor_slice_co.paa"
+			};
+		};
+	};
+	class Burtovski_Armor_Rifleman : OPTRE_UNSC_M52D_Armor_Rifleman
+	{
+		dlc = "PHarmor";
+		author = "Project Harvest Mod Team";
+		displayName = "M52D Body Armor (Burtovski)";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			//"A_Base",
+			"A_Ghillie",
+			//"A_ChestArmor", 
+			//"A_KneesLeft", 
+			//"A_KneesRight", 
+			"A_KneesMarLeft", 
+			"A_KneesMarRight", 
+			//"A_ODST", 
+			//"A_ShinArmorLeft", 
+			//"A_ShinArmorRight", 
+			//"A_TacPad", 
+			//"A_ThighArmorLeft", 
+			//"A_ThighArmorRight", 
+			//"AS_BaseLeft", 
+			//"AS_BaseRight", 
+			"AS_LargeLeft", 
+			"AS_LargeRight", 
+			"AS_MediumLeft", 
+			"AS_MediumRight", 
+			//"AS_ODSTCQBLeft", 
+			"AS_ODSTCQBRight", 
+			"AS_ODSTLeft", 
+			//"AS_ODSTRight", 
+			"AS_ODSTSniperLeft", 
+			"AS_ODSTSniperRight", 
+			"AS_SmallLeft",  
+			"AS_SmallRight", 
+			"AP_Canteen", 
+			"AP_GL", 
+			"AP_Knife", 
+			"AP_MGThigh", 
+			//"AP_AR", 
+			"AP_BR", 
+			"AP_Pack", 
+			"AP_Pistol", 
+			"AP_Rounds", 
+			"AP_SG", 
+			"AP_SMG", 
+			"AP_Sniper", 
+			"AP_Thigh", 
+			//"AP_Frag", 
+			//"AP_Smoke", 
+			"APO_AR", 
+			"APO_BR", 
+			"APO_Knife", 
+			"APO_SMG", 
+			"APO_Sniper", 
+			"CustomKit_Scorch"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"PHarmor\data\Army\data\vest_odst_co.paa",
+			"PHarmor\data\army\data\armor_odst_co.paa",
+			"PHarmor\data\armor\customlegs_soldner_co.paa",
+			"PHarmor\data\army\data\ghillie_woodland_co.paa",
+			"PHarmor\data\armor\customarmor_burtovski_co.paa"
+		};
+		class ItemInfo :ItemInfo
+		{
+			hiddenSelections[] =
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				//"A_Base",
+				"A_Ghillie",
+				//"A_ChestArmor", 
+				//"A_KneesLeft", 
+				//"A_KneesRight", 
+				"A_KneesMarLeft", 
+				"A_KneesMarRight", 
+				//"A_ODST", 
+				//"A_ShinArmorLeft", 
+				//"A_ShinArmorRight", 
+				//"A_TacPad", 
+				//"A_ThighArmorLeft", 
+				//"A_ThighArmorRight", 
+				//"AS_BaseLeft", 
+				//"AS_BaseRight", 
+				"AS_LargeLeft", 
+				"AS_LargeRight", 
+				"AS_MediumLeft", 
+				"AS_MediumRight", 
+				//"AS_ODSTCQBLeft", 
+				"AS_ODSTCQBRight", 
+				"AS_ODSTLeft", 
+				//"AS_ODSTRight", 
+				"AS_ODSTSniperLeft", 
+				"AS_ODSTSniperRight", 
+				"AS_SmallLeft",  
+				"AS_SmallRight", 
+				"AP_Canteen", 
+				"AP_GL", 
+				"AP_Knife", 
+				"AP_MGThigh", 
+				//"AP_AR", 
+				"AP_BR", 
+				"AP_Pack", 
+				"AP_Pistol", 
+				"AP_Rounds", 
+				"AP_SG", 
+				"AP_SMG", 
+				"AP_Sniper", 
+				"AP_Thigh", 
+				//"AP_Frag", 
+				//"AP_Smoke", 
+				"APO_AR", 
+				"APO_BR", 
+				"APO_Knife", 
+				"APO_SMG", 
+				"APO_Sniper", 
+				"CustomKit_Scorch"
+			};
+			hiddenSelectionsTextures[] =
+			{
+				"PHarmor\data\Army\data\vest_odst_co.paa",
+				"PHarmor\data\army\data\armor_odst_co.paa",
+				"PHarmor\data\armor\customlegs_soldner_co.paa",
+				"PHarmor\data\army\data\ghillie_woodland_co.paa",
+				"PHarmor\data\armor\customarmor_burtovski_co.paa"
 			};
 		};
 	};
