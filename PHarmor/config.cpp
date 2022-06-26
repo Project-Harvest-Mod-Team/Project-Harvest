6ce94e02b2170aaa46a21f45937be714f7b3898c
VorpalHotdog <zacharettig@gmail.com>
2022-06-25 20:45:15 -0500

    Added raider armor + updated soldner & burt

--- PHarmor/config.cpp
+++ PHarmor/config.cpp
@@ -4629,4 +4629,141 @@ class CfgWeapons
 			};
 		};
 	};
+	class Riot_Armor_Rifleman : OPTRE_UNSC_M52D_Armor_Rifleman
+	{
+		dlc = "PHarmor";
+		author = "Project Harvest Mod Team";
+		displayName = "M52D Body Armor (Riot)";
+		hiddenSelections[] =
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			//"A_Base",
+			"A_Ghillie",
+			//"A_ChestArmor", 
+			//"A_KneesLeft", 
+			//"A_KneesRight", 
+			"A_KneesMarLeft", 
+			"A_KneesMarRight", 
+			//"A_ODST", 
+			//"A_ShinArmorLeft", 
+			//"A_ShinArmorRight", 
+			//"A_TacPad", 
+			//"A_ThighArmorLeft", 
+			//"A_ThighArmorRight", 
+			//"AS_BaseLeft", 
+			//"AS_BaseRight", 
+			"AS_LargeLeft", 
+			"AS_LargeRight", 
+			"AS_MediumLeft", 
+			"AS_MediumRight", 
+			//"AS_ODSTCQBLeft", 
+			"AS_ODSTCQBRight", 
+			"AS_ODSTLeft", 
+			//"AS_ODSTRight", 
+			"AS_ODSTSniperLeft", 
+			"AS_ODSTSniperRight", 
+			"AS_SmallLeft",  
+			"AS_SmallRight", 
+			"AP_Canteen", 
+			"AP_GL", 
+			"AP_Knife", 
+			"AP_MGThigh", 
+			"AP_AR", 
+			"AP_BR", 
+			"AP_Pack", 
+			"AP_Pistol", 
+			"AP_Rounds", 
+			"AP_SG", 
+			"AP_SMG", 
+			"AP_Sniper", 
+			//"AP_Thigh", 
+			"AP_Frag", 
+			"AP_Smoke", 
+			//"APO_AR", 
+			"APO_BR", 
+			//"APO_Knife", 
+			"APO_SMG", 
+			"APO_Sniper", 
+			"CustomKit_Scorch"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"PHarmor\data\Army\data\vest_odst_co.paa",
+			"PHarmor\data\army\data\armor_odst_co.paa",
+			"PHarmor\data\armor\customlegs_riot_co.paa",
+			"PHarmor\data\army\data\ghillie_woodland_co.paa",
+			"PHarmor\data\armor\customarmor_riot_CO.paa"
+		};
+		class ItemInfo :ItemInfo
+		{
+			hiddenSelections[] =
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				//"A_Base",
+				"A_Ghillie",
+				//"A_ChestArmor", 
+				//"A_KneesLeft", 
+				//"A_KneesRight", 
+				"A_KneesMarLeft", 
+				"A_KneesMarRight", 
+				//"A_ODST", 
+				//"A_ShinArmorLeft", 
+				//"A_ShinArmorRight", 
+				//"A_TacPad", 
+				//"A_ThighArmorLeft", 
+				//"A_ThighArmorRight", 
+				//"AS_BaseLeft", 
+				//"AS_BaseRight", 
+				"AS_LargeLeft", 
+				"AS_LargeRight", 
+				"AS_MediumLeft", 
+				"AS_MediumRight", 
+				//"AS_ODSTCQBLeft", 
+				"AS_ODSTCQBRight", 
+				"AS_ODSTLeft", 
+				//"AS_ODSTRight", 
+				"AS_ODSTSniperLeft", 
+				"AS_ODSTSniperRight", 
+				"AS_SmallLeft",  
+				"AS_SmallRight", 
+				"AP_Canteen", 
+				"AP_GL", 
+				"AP_Knife", 
+				"AP_MGThigh", 
+				"AP_AR", 
+				"AP_BR", 
+				"AP_Pack", 
+				"AP_Pistol", 
+				"AP_Rounds", 
+				"AP_SG", 
+				"AP_SMG", 
+				"AP_Sniper", 
+				//"AP_Thigh", 
+				"AP_Frag", 
+				"AP_Smoke", 
+				//"APO_AR", 
+				"APO_BR", 
+				//"APO_Knife", 
+				"APO_SMG", 
+				"APO_Sniper", 
+				"CustomKit_Scorch"
+			};
+			hiddenSelectionsTextures[] =
+			{
+				"PHarmor\data\Army\data\vest_odst_co.paa",
+				"PHarmor\data\army\data\armor_odst_co.paa",
+				"PHarmor\data\armor\customlegs_riot_co.paa",
+				"PHarmor\data\army\data\ghillie_woodland_co.paa",
+				"PHarmor\data\armor\customarmor_riot_CO.paa"
+			};
+		};
+	};
 };
