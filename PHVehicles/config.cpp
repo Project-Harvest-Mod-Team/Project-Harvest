497cb8523c2579838c02320d3c632629e010e690
Tom Soldner <tomsoldner4@gmail.com>
2023-02-06 13:29:27 -0500

    3.2.1.1
    Soldner fix

--- PHVehicles/config.cpp
+++ PHVehicles/config.cpp
@@ -27,20 +27,12 @@ class CfgVehicles
 		visualTargetSize = 0.6;
 		irTargetSize = 0.6;
 	}
-	class PHRP_Foulke_Bayonet_F: PHRP_Bayonet_Base
-	{
-		scope = 2;
-		scopeCurator = 2;
-		displayName = "Foulke's Bayonet";
-		hiddenSelections[] = {"Camo1","Camo2","number_01","number_02","number_03"};
-		hiddenSelectionsTextures[] = {"PHVehicles\Data\Foulke_01_co.paa","PHVehicles\data\Strigon_02_co.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_00_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa"};
-	};
 	class PHRP_Raider_Bayonet_F: PHRP_Bayonet_Base
 	{
 		scope = 2;
 		scopeCurator = 2;
 		displayName = "Raider's Bayonet";
 		hiddenSelections[] = {"Camo1","Camo2","number_01","number_02","number_03"};
-		hiddenSelectionsTextures[] = {"PHVehicles\Data\Raider_01_co.paa","PHVehicles\data\Strigon_02_co.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_00_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_02_ca.paa"};
+		hiddenSelectionsTextures[] = {"PHVehicles\Data\Raider_01_co.paa","PHVehicles\data\Strigon_02_co.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_00_ca.paa","a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa"};
 	};
 };
